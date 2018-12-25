d96af3edf4efad5b4f1d072c0e67e9d2c6f158b6
s-matsuguma <s-matsuguma@netyear.net>
2018-12-25 13:15:30 +0900

    first commit

--- customActivity.js
+++ customActivity.js
@@ -9,7 +9,8 @@ define([
     var payload = {};
     var lastStepEnabled = false;
     var steps = [ // initialize to the same value as what's set in config.json for consistency
-        { "label": "Step 1", "key": "step1" }
+        { "label": "Step 1", "key": "step1" },
+        { "label": "Step 2", "key": "step2" }
     ];
     var currentStep = steps[0].key;
 
@@ -126,10 +127,15 @@ define([
                 $('#step1').show();
                 connection.trigger('updateButton', {
                 	button: 'next',
-                	text: 'done',
+                	text: 'next',
                     visible: true
                 });
+                connection.trigger('updateButton', {
+                    button: 'back',
+                    visible: false
+                });
                 break;
+                
             case 'step2':
                 $('#step2').show();
                 connection.trigger('updateButton', {
@@ -138,10 +144,14 @@ define([
                 });
                 connection.trigger('updateButton', {
                     button: 'next',
-                    text: 'next',
+                    text: 'done',
                     visible: true
                 });
                 break;
+                
+                
+                
+                
             case 'step3':
                 $('#step3').show();
                 connection.trigger('updateButton', {
